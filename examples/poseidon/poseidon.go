ff47d1e56e24bcd459e21815dba3ed347ce3e83b
Philipp Sippl <philsippl@gmail.com>
2022-10-24 17:02:35 +0200

    poseidon1

--- examples/poseidon/poseidon.go
+++ examples/poseidon/poseidon.go
@@ -9,6 +9,8 @@ type Poseidon struct {
 	nFullRounds  int
 	data         []frontend.Variable
 	api          frontend.API
+	constants    [][]frontend.Variable
+	mds          [][]frontend.Variable
 }
 
 func sbox(api frontend.API, v frontend.Variable) frontend.Variable {
@@ -17,58 +19,71 @@ func sbox(api frontend.API, v frontend.Variable) frontend.Variable {
 	return api.Mul(v, v4)
 }
 
-func applyMDS(api frontend.API, state []frontend.Variable) []frontend.Variable {
-	if len(state) != len(MDS) {
+func (h *Poseidon) applyMDS(api frontend.API, state []frontend.Variable) []frontend.Variable {
+	if len(state) != len(h.mds) {
 		panic("state and MDS size do not match")
 	}
 
 	var mds []frontend.Variable
-	for i := 0; i < len(MDS); i += 1 {
+	for i := 0; i < len(h.mds); i += 1 {
 		var sum frontend.Variable = 0
-		for j := 0; j < len(MDS[i]); j += 1 {
-			sum = api.Add(sum, api.Mul(state[j], MDS[i][j]))
+		for j := 0; j < len(h.mds[i]); j += 1 {
+			sum = api.Add(sum, api.Mul(state[j], h.mds[i][j]))
 		}
 		mds = append(mds, sum)
 	}
 	return mds
 }
 
-func halfRound(api frontend.API, round int, state []frontend.Variable) []frontend.Variable {
-	if len(state) != len(CONSTANTS[round]) {
+func (h *Poseidon) halfRound(api frontend.API, round int, state []frontend.Variable) []frontend.Variable {
+	if len(state) != len(h.constants[round]) {
 		panic("state and round constants size do not match")
 	}
 
 	for i := 0; i < len(state); i += 1 {
-		state[i] = api.Add(state[i], CONSTANTS[round][i])
+		state[i] = api.Add(state[i], h.constants[round][i])
 	}
 
 	state[0] = sbox(api, state[0])
 
-	return applyMDS(api, state)
+	return h.applyMDS(api, state)
 }
 
-func fullRound(api frontend.API, round int, state []frontend.Variable) []frontend.Variable {
-	if len(state) != len(CONSTANTS[round]) {
+func (h *Poseidon) fullRound(api frontend.API, round int, state []frontend.Variable) []frontend.Variable {
+	if len(state) != len(h.constants[round]) {
 		panic("state and round constants size do not match")
 	}
 
 	for i := 0; i < len(state); i += 1 {
-		state[i] = api.Add(state[i], CONSTANTS[round][i])
+		state[i] = api.Add(state[i], h.constants[round][i])
 	}
 
 	for i := 0; i < len(state); i += 1 {
 		state[i] = sbox(api, state[i])
 	}
 
-	return applyMDS(api, state)
+	return h.applyMDS(api, state)
 }
 
-func NewPoseidon(api frontend.API) Poseidon {
+func NewPoseidon1(api frontend.API) Poseidon {
+	return Poseidon{
+		nFullRounds:  4,
+		nTotalRounds: 63,
+		data:         []frontend.Variable{0},
+		api:          api,
+		constants:    CONSTANTS1,
+		mds:          MDS1,
+	}
+}
+
+func NewPoseidon2(api frontend.API) Poseidon {
 	return Poseidon{
 		nFullRounds:  4,
 		nTotalRounds: 64,
 		data:         []frontend.Variable{0},
 		api:          api,
+		constants:    CONSTANTS,
+		mds:          MDS,
 	}
 }
 
@@ -84,9 +99,9 @@ func (h *Poseidon) Sum() frontend.Variable {
 	state := h.data
 	for i := 0; i < h.nTotalRounds+1; i += 1 {
 		if i < h.nFullRounds || i > (h.nTotalRounds-h.nFullRounds) {
-			state = fullRound(h.api, i, state)
+			state = h.fullRound(h.api, i, state)
 		} else {
-			state = halfRound(h.api, i, state)
+			state = h.halfRound(h.api, i, state)
 		}
 	}
 	return state[0]
--- examples/poseidon/poseidon_test.go
+++ examples/poseidon/poseidon_test.go
@@ -23,15 +23,26 @@ import (
 	"github.com/consensys/gnark/test"
 )
 
-type TestPoseidonCircuit struct {
+type TestPoseidonCircuit1 struct {
+	Input frontend.Variable `gnark:"input"`
+	Hash  frontend.Variable `gnark:",public"`
+}
+
+type TestPoseidonCircuit2 struct {
 	Left  frontend.Variable `gnark:"left"`
 	Right frontend.Variable `gnark:"right"`
 	Hash  frontend.Variable `gnark:",public"`
 }
 
-// Define declares the circuit constraints
-func (circuit *TestPoseidonCircuit) Define(api frontend.API) error {
-	poseidon := NewPoseidon(api)
+func (circuit *TestPoseidonCircuit1) Define(api frontend.API) error {
+	poseidon := NewPoseidon1(api)
+	poseidon.Write(circuit.Input)
+	api.AssertIsEqual(circuit.Hash, poseidon.Sum())
+	return nil
+}
+
+func (circuit *TestPoseidonCircuit2) Define(api frontend.API) error {
+	poseidon := NewPoseidon2(api)
 	poseidon.Write(circuit.Left, circuit.Right)
 	api.AssertIsEqual(circuit.Hash, poseidon.Sum())
 	return nil
@@ -40,15 +51,21 @@ func (circuit *TestPoseidonCircuit) Define(api frontend.API) error {
 func TestPoseidon(t *testing.T) {
 	assert := test.NewAssert(t)
 
-	var cubicCircuit TestPoseidonCircuit
+	var circuit1 TestPoseidonCircuit1
+	assert.ProverSucceeded(&circuit1, &TestPoseidonCircuit1{
+		Input: 0,
+		Hash:  hex("0x2a09a9fd93c590c26b91effbb2499f07e8f7aa12e2b4940a3aed2411cb65e11c"),
+	}, test.WithBackends(backend.GROTH16), test.WithCurves(ecc.BN254))
 
-	assert.ProverSucceeded(&cubicCircuit, &TestPoseidonCircuit{
+	var circuit2 TestPoseidonCircuit2
+
+	assert.ProverSucceeded(&circuit2, &TestPoseidonCircuit2{
 		Left:  0,
 		Right: 0,
 		Hash:  hex("0x2098f5fb9e239eab3ceac3f27b81e481dc3124d55ffed523a839ee8446b64864"),
 	}, test.WithBackends(backend.GROTH16), test.WithCurves(ecc.BN254))
 
-	assert.ProverSucceeded(&cubicCircuit, &TestPoseidonCircuit{
+	assert.ProverSucceeded(&circuit2, &TestPoseidonCircuit2{
 		Left:  31213,
 		Right: 132,
 		Hash:  hex("0x303f59cd0831b5633bcda50514521b33776b5d4280eb5868ba1dbbe2e4d76ab5"),
